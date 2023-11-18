46015d320d2973bef116613c30047862c80b6113
Musa Sofiyah <musasophiat@gmail.com>
2023-11-18 20:39:13 +0100

    Update index.js
    checking for some functionality

--- src/index.js
+++ src/index.js
@@ -2,8 +2,6 @@ function refreshWeather(response) {
   let temperature = document.querySelector("#temperature");
   let cityName = document.querySelector("#cityName");
   cityName.innerHTML = response.data.city;
-//   console.log(cityName);
-//   alert(cityName);
   temperature.innerHTML = Math.round(response.data.temperature.current);
 }
 
@@ -11,7 +9,7 @@ function searchCity(city) {
   let apiKey = "b2a5adcct04b33178913oc335f405433";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
   let cityName = document.querySelector("#cityName");
-  
+
   console.log(apiUrl);
   axios.get(apiUrl).then(refreshWeather);
 }
@@ -29,4 +27,3 @@ let searchFormElement = document.querySelector("#searchForm");
 // console.log(searchFormElement);
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 searchCity("Paris");
-
