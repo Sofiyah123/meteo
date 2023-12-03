b68ea0c8c316af65a708659aaee0b90469aaf9c7
Sofiyah123 <musasophiat@gmail.com>
2023-12-03 14:42:51 +0100

    Update index.js
    Added function to display daily fore cast

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ function searchCity(city) {
   let apiKey = "b2a5adcct04b33178913oc335f405433";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
   let cityName = document.querySelector("#cityName");
-
   console.log(apiUrl);
   axios.get(apiUrl).then(refreshWeather);
 }
@@ -20,10 +19,35 @@ function handleSearchSubmit(event) {
   console.log(searchInput.value);
   let cityName = document.querySelector("#cityName");
   cityName.innerHTML = searchInput.value;
-  // call the API
   searchCity(searchInput.value);
 }
+
+function displayForecast() {
+  let weatherForecast = document.querySelector("#weatherForecast");
+  let days = ["Tue", "Wed", "Thu", "Fri", "Sat"];
+  let forecastHtml = " ";
+  days.forEach(function (day) {
+    forecastHtml =
+      forecastHtml +
+      `
+      <div class="weatherForecast-day">
+                <div class="weather-forecast-date">${day}</div>
+                <div class="weather-forecast-icon">☀</div>
+                <div class="weather-forecast-temperatures">
+                  <div class="weather-forecast-temperature">
+                    <strong>15°</strong>
+                  </div>
+                  <div class="weather-forecast-temperature"> 9°</div>
+                </div>
+        </div>
+        
+        
+            `;
+  });
+  weatherForecast.innerHTML = forecastHtml;
+}
+
 let searchFormElement = document.querySelector("#searchForm");
-// console.log(searchFormElement);
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 searchCity("Paris");
+displayForecast();
